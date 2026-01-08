f29ff06c69fbe615952a5dd21fb524a23b3bccfc
HasaliM23 <hasali2000pm@gmail.com>
2026-01-07 16:58:50 -0800

    Update

--- src/test/java/LoginFormTesting/UnregisteredEmailLoginTest.java
+++ src/test/java/LoginFormTesting/UnregisteredEmailLoginTest.java
@@ -1,6 +1,59 @@
 package LoginFormTesting;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class UnregisteredEmailLoginTest {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeMethod
+    public void openLink(){
+        driver = new ChromeDriver();
+        driver.get("https://practice.qabrains.com/");
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    }
+
+    @Test
+
+    public void verifyIncorrectPasswordShowsError(){
+
+        WebElement email = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='email']")));
+        email.sendKeys("test@gmail.com");
+
+        WebElement psw = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='password']")));
+        psw.sendKeys("Password123");
+
+        WebElement signIn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[.='Login']")));
+        signIn.click();
+
+        // Wait for error message
+        WebElement errorMsg = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//span[.='Your email is invalid!']")
+        ));
+
+// ASSERT error message is displayed
+        Assert.assertTrue(errorMsg.isDisplayed(),
+                "Error message not shown for incorrect password — BUG!");
+
+
+
+
+
+
+    }
 
 
 }
